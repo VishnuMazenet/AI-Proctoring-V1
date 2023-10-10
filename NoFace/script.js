28deb12160e604086f7ee00ce8fb1c1f7f979971
VishnuMazenet <jr_developer2@mazenetsolution.com>
2023-10-10 12:34:13 +0530

    Added comments for understanding

--- NoFace/script.js
+++ NoFace/script.js
@@ -1,3 +1,4 @@
+// Wait for the DOM to be ready
 $(document).ready(function () {
     // Get the video element and canvas element
     var video = document.getElementById('video');
@@ -8,13 +9,7 @@ $(document).ready(function () {
     let model, faces;
     var lastCaptureTime = 0;
     let num = 0;
-    let aud = 1;
     let warning;
-
-    // Variables for frequency detection
-    var audioContext, analyser, dataArray, soundDetected;
-    const MIN_FREQUENCY = 10;
-    const MAX_FREQUENCY = 4000;
 
     // Load the face detection model
     async function loadFaceDetectionModel() {
@@ -24,46 +19,70 @@ $(document).ready(function () {
     // Start the video stream and face detection
     async function startVideoStream() {
         if (navigator.mediaDevices.getUserMedia) {
+            // Request access to the user's webcam
             const stream = await navigator.mediaDevices.getUserMedia({
                 video: true
             });
-            video.srcObject = stream;
-            video.addEventListener('loadeddata', detectFaces); // Wait for the video to finish loading
 
+            // Set the video source to the webcam stream
+            video.srcObject = stream;
+
+            // Wait for the video to finish loading
+            video.addEventListener('loadeddata', detectFaces);
         }
     }
+
+    // Perform face detection on the video frames
     async function detectFaces() {
+        // Use the face detection model to estimate faces in the video frames
         faces = await model.estimateFaces(video);
+
+        // Get the current time
         var currentTime = Date.now();
+
+        // Check if no face is detected or multiple faces are detected
         if ((faces.length === 0 || faces.length > 1) && (currentTime - lastCaptureTime > 5000)) {
-            // Show warning message
+            // Show a warning message
             if (faces.length === 0) {
                 showWarning("No face detected");
                 warning = "No face detected";
             }
+
+            // Capture an image
             captureImage(warning);
+
+            // Update the last capture time
             lastCaptureTime = currentTime;
         }
+
+        // Continue detecting faces in the next frame
         requestAnimationFrame(detectFaces);
     }
 
-    // Show warning message
+    // Show a warning message
     function showWarning(message) {
+        // Display the warning message on the screen
         $('.warning').html(message);
         $('.warning').show();
+
+        // Hide the warning message after 3 seconds
         setTimeout(function () {
             $('.warning').hide();
         }, 3000);
     }
-    // Capture image and send to the server
+
+    // Capture an image and send it to the server
     function captureImage(reason) {
-        // Draw the current video frame onto the canvas
+        // Draw the current frame of the video onto the canvas
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
         // Get the base64-encoded image data from the canvas
         var imageData = canvas.toDataURL('image/png');
+
+        // Increment the image ID
         ++num;
-        // Create the JSON object with image information
+
+        // Create a JSON object with image information
         var imageInfo = {
             id: num,
             name: reason,
@@ -81,6 +100,8 @@ $(document).ready(function () {
             success: function (response) {
                 console.log('Image captured and sent to the server.');
                 console.log(response);
+
+                // Update the number of captured images
                 $('.num').html(num);
             }
         });
